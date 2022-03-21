ac5f08b3ca9e78e0ad34e933dfe1cbc7989cc945
Supersagee <williamdsage@gmail.com>
2022-03-21 16:16:49 -0500

    complete

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ console.log(
 );
 
 // Exercise 1
+const firstName = `Billy`;
+let lastName = `Sage`;
+var age = 30;
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+let myCity = `Chaska`;
+let favPastTime = `Sim Racing`;
+
+let myStory = `Hello, my name is ${fullName} and I am ${30} years old.
+I have lived in ${myCity} for the past 5 years.
+My favorite hobby is ${favPastTime}.`
+
+console.log(myStory);
